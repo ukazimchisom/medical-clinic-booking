760d5532bcf0bdbdb52899e764e106ad9475b1dd
ukazim chisom <Ukazim chisom@gmail.com>
2026-03-16 22:27:59 +0100

    feat: implement admin edit doctor page

--- src/services/doctor-service.ts
+++ src/services/doctor-service.ts
@@ -36,3 +36,34 @@ export async function deleteDoctor(id: string) {
 
   return true;
 }
+
+export async function getDoctorById(id: string) {
+  const { data, error } = await supabase
+    .from("doctors")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+}
+
+export async function updateDoctor(
+  id: string,
+  data: {
+    name: string;
+    specialty: string;
+    photo?: string;
+  },
+) {
+  const { error } = await supabase.from("doctors").update(data).eq("id", id);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return true;
+}
